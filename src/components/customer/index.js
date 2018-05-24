08dca553d116a3f0121b77447ca4331f98741b03
sri1991 <bharanisrinivas91@gmail.com>
2018-05-24 17:06:06 +0530

    Added react lodable for right side component

---
+++ src/components/customer/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class CustomerComponent extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+       Customer 
+      </div>
+    );
+  }
+}
+
+export default CustomerComponent;
